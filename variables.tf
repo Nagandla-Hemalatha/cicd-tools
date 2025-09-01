2ae5d9065044ed391f4bcef1b14f2de7182e9574
Nagandla-Hemalatha <nagandla2001@gmail.com>
2025-09-01 07:56:58 +0530

    cicd tools instance creation

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "hemalatha.site"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z040983115KD520HXV305"
+  description = "description"
+}
