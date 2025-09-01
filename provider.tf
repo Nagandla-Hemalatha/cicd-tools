a73649b49aeeb88f95835d3defb924f76beb1794
Nagandla-Hemalatha <nagandla2001@gmail.com>
2025-09-01 07:58:21 +0530

    cicd tools instance creation

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "latest-84s-remote-state-dev"
+    bucket = "latest-hema-remote-state-dev"
     key    = "roboshop-dev-cicd"
     region = "us-east-1"
     encrypt        = true
