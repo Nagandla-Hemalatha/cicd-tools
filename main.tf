467f1554aa6295bfad54c7aa8c8158aae1a4a32a
Nagandla-Hemalatha <nagandla2001@gmail.com>
2025-09-20 22:35:50 +0530

    cicd tools

--- main.tf
+++ main.tf
@@ -68,6 +68,53 @@ resource "aws_security_group" "main" {
   )
 }
 
+
+resource "aws_instance" "sonarqube" {
+  ami           = local.ami_id
+  #instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all_sonarqube.id]
+  instance_type = "t3.medium"
+  # need more for terraform
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3" # or "gp2", depending on your preference
+  }
+  user_data = file("sonar.sh")
+  #iam_instance_profile = "TerraformAdmin"
+  tags = {
+     Name = "${var.project}-${var.environment}-sonarqube"
+  }
+}
+
+resource "aws_security_group" "allow_all_sonarqube" {
+    name        = "allow_all_sonarqube"
+    description = "allow all traffic"
+
+    ingress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    lifecycle {
+      create_before_destroy = true
+    }
+
+    tags = {
+        Name = "allow-all-sonarqube"
+    }
+}
+
+
 resource "aws_route53_record" "jenkins" {
   zone_id = var.zone_id
   name    = "jenkins.${var.zone_name}"
@@ -83,5 +130,14 @@ resource "aws_route53_record" "jenkins-agent" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.jenkins_agent.private_ip]
+  allow_overwrite = true
+}
+
+resource "aws_route53_record" "sonarqube" {
+  zone_id = var.zone_id
+  name    = "sonarqube.${var.zone_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.sonarqube.private_ip]
   allow_overwrite = true
 }
